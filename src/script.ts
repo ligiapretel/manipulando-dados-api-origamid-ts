d4652a6dea0e67d39c8d55489f0a28bc4281c82c
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2023-10-16 14:36:25 -0300

    feat: Puxando dados da API

---
+++ src/script.ts
@@ -0,0 +1,9 @@
+import fetchData from "./fetchData.js";
+
+
+async function handleData(){
+    const data = await fetchData('https://api.origamid.dev/json/transacoes.json');
+    console.log(data);
+}
+
+handleData();
